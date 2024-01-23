aa0034edfcce09d823d4d88936ae56f0f7f07a43
JernejSlo <jernejkoprivnikar.slo@gmail.com>
2024-01-23 11:49:08 +0100

    more content

--- Values/Trails.js
+++ Values/Trails.js
@@ -24,6 +24,84 @@ const trails = [
             }
         ]},
     {
+        id: 2312312,
+        title: "Riverside Path",
+        description: "A relaxing walk along a scenic river, with flat terrain suitable for beginners and casual hikers.",
+        img: require("../assets/I1.png"), // you would replace this with the actual path to the image asset
+        bg: require("../assets/BG1.png"), // you would replace this with the actual path to the image asset
+        time: 1,
+        values: [
+            {
+                "title": "Calories/h",
+                "value": "400/h"
+            },
+            {
+                "title": "Total",
+                "value": "2h"
+            },
+            {
+                "title": "Downloads",
+                "value": "1548"
+            },
+            {
+                "title": "Start",
+                "value": "lvl 0"
+            }
+        ]
+    },
+    {
+        id: 1231412341254,
+        title: "Coastal Cliffside Run",
+        description: "Breathtaking seaside path for runners and walkers, featuring gentle elevations and panoramic ocean views.",
+        img: require("../assets/I2.png"), // you would replace this with the actual path to the image asset
+        bg: require("../assets/BG2.png"), // you would replace this with the actual path to the image asset
+        time: 1,
+        values: [
+            {
+                "title": "Calories/h",
+                "value": "500/h"
+            },
+            {
+                "title": "Total",
+                "value": "1.5h"
+            },
+            {
+                "title": "Downloads",
+                "value": "2300"
+            },
+            {
+                "title": "Start",
+                "value": "lvl 2"
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Morning city run",
+        description: "Calm morning in the city, the sun is glistening and the city feels alive.",
+        img: require("../assets/I3.png"), // you would replace this with the actual path to the image asset
+        bg: require("../assets/BG3.png"), // you would replace this with the actual path to the image asset
+        time: 1,
+        values: [
+            {
+                "title": "Calories/h",
+                "value": "400/h"
+            },
+            {
+                "title": "Total",
+                "value": "1h"
+            },
+            {
+                "title": "Downloads",
+                "value": "1000"
+            },
+            {
+                "title": "Start",
+                "value": "lvl 1"
+            }
+        ]
+    },
+    {
         id: 2,
         title: "Summer Beach Run Course",
         description: "Enjoy this scenic hour long beach running course made specially for you!",
@@ -94,7 +172,33 @@ const trails = [
                     "title": "Start",
                     "value": "lvl 3"
                 }]
-        }
-    ]
+        },{
+        id: 4,
+        title: "Beachside Bliss Walk",
+        description: "A scenic tropical trail along the beach, surrounded by palm trees and the gentle sound of ocean waves.",
+        img: require("../assets/I4.png"), // replace with the actual path to the icon image asset
+        bg: require("../assets/BG4.png"), // replace with the actual path to the background image asset
+        time: 1,
+        values: [
+            {
+                "title": "Calories/h",
+                "value": "450/h"
+            },
+            {
+                "title": "Total",
+                "value": "2h"
+            },
+            {
+                "title": "Downloads",
+                "value": "3675"
+            },
+            {
+                "title": "Start",
+                "value": "lvl 1"
+            }
+        ]
+    }
+
+]
 
 export default trails
---
+++ Values/Training.js
@@ -0,0 +1,31 @@
+const training = [
+    {
+        id: 1,
+        title: "Treadmill Sprints for Explosiveness",
+        description: "This treadmill-focused training session is designed to build your explosive running power with high-speed intervals and incline challenges.",
+        img: require("../assets/IT1.png"), // replace with the actual path to the icon image asset
+        bg: require("../assets/BGT1.png"), // replace with the actual path to the background image asset
+        time: 0.5,
+        values: [
+            {
+                "title": "Calories/h",
+                "value": "600/h"
+            },
+            {
+                "title": "Total",
+                "value": "30m"
+            },
+            {
+                "title": "Downloads",
+                "value": "1820"
+            },
+            {
+                "title": "Start",
+                "value": "lvl 3"
+            }
+        ]
+    }
+
+]
+
+export default training
